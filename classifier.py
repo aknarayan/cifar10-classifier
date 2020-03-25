75d31121bc5b517aa6bfb171a93b1aabb2886d24
Akshay Narayan <akshay.narayan5599@gmail.com>
2020-03-25 09:36:41 +0000

    Defined convolutional neural network

--- classifier.py
+++ classifier.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import torch.nn as nn
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -19,7 +20,7 @@ classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 
 
 def imshow(img):
-    img = img / 2 + 0.5 # un-normalise
+    img = img / 2 + 0.5  # un-normalise
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
@@ -29,3 +30,33 @@ dataiter = iter(training_loader)
 images, labels = dataiter.next()
 imshow(torchvision.utils.make_grid(images))
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = x.view(-1, 16 * 5 * 5)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
