edcac79ab41d71b6a505c4ad86ed71e3de7ab245
Akshay Narayan <akshay.narayan5599@gmail.com>
2020-03-25 09:25:50 +0000

    Display training images

--- classifier.py
+++ classifier.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -14,3 +16,16 @@ test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=Tru
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=4, shuffle=False)
 
 classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+
+def imshow(img):
+    img = img / 2 + 0.5 # un-normalise
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+dataiter = iter(training_loader)
+images, labels = dataiter.next()
+imshow(torchvision.utils.make_grid(images))
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
