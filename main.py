4deeace30e05a46176214d8e31299d215732482a
Akshay Narayan <akshay.narayan5599@gmail.com>
2020-03-25 15:38:12 +0000

    Parse command line arguments

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from preprocessor import Preprocessor
 from classifier import Classifier
+import sys
 
 
 def main():
-    preprocessor = Preprocessor(batch_size=4)
+    batch_size = int(sys.argv[1])
+    save_model = True if sys.argv[2] == "True" else False
+
+    preprocessor = Preprocessor(batch_size)
     preprocessor.load()
     preprocessor.show_examples()
 
@@ -13,7 +17,7 @@ def main():
     batch_size = preprocessor.batch_size
 
     classifier = Classifier()
-    classifier.train(training_loader, save_model=False)
+    classifier.train(training_loader, save_model)
     classifier.evaluate(test_loader, classes, batch_size)
 
 
