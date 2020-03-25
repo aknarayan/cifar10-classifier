567f839e1cbbb04d00f24ba6226111479088562b
Akshay Narayan <akshay.narayan5599@gmail.com>
2020-03-25 13:06:22 +0000

    Evaluate on test set and print performance metrics

--- classifier.py
+++ classifier.py
@@ -5,6 +5,7 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.optim as optim
+import sklearn.metrics as metrics
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -76,3 +77,24 @@ for epoch in range(2):
         if i % 1000 == 0:
             print("Epoch {}, Image {}: loss = {}".format(epoch + 1, i, loss.item()))
 print("Finished training")
+
+ground_truths = []
+predictions = []
+with torch.no_grad():
+    for data in test_loader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        for j in range(4):
+            ground_truths.append(classes[labels[j]])
+            predictions.append(classes[predicted[j]])
+confusion_matrix = metrics.confusion_matrix(ground_truths, predictions)
+accuracy = metrics.accuracy_score(ground_truths, predictions)
+(p, r, f1, supp) = metrics.precision_recall_fscore_support(ground_truths, predictions, average='macro')
+print("Confusion Matrix:")
+print(confusion_matrix)
+print("Accuracy: {}".format(accuracy))
+print("Precision: {}".format(p))
+print("Recall: {}".format(r))
+print("F1 Score: {}".format(f1))
+print("Support: {}".format(supp))
